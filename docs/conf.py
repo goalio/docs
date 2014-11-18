8195d058dee9f4af04e1a5101190e9a1039604b5
Stefan Kleff <s.kleff@goalio.de>
2014-11-18 16:21:22 +0100

    dfh

--- docs/conf.py
+++ docs/conf.py
@@ -103,13 +103,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'goalio'
+#html_theme = 'goalio'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "sticky_navigation" : "true"
+    "sticky_navigation" : "true",
+    "display_github" : "False",
+    "show_source" : "False"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -117,7 +119,7 @@ html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'goalio Dokumentation'
+html_title = 'goalio'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
